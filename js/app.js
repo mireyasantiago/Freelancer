c634df351f769d5feaed0be0f572fb63592c3bac
mireyasantiago <mireyasantiago105@gmail.com>
2017-05-04 01:04:25 -0500

    comenzando estilo y estructura

--- js/app.js
+++ js/app.js
@@ -1,3 +1,23 @@
 /*
  * Archivo principal de JS
  */
+var modal = document.getElementById("modalArea");
+var imagenModal = document.getElementById("modalImage");
+var boton = document.getElementById("close");
+var fotos = document.getElementsByClassName("fotito");
+
+function mostrarModal(){
+    console.log(modal);
+    modal.style.display = "block";
+    imagenModal.src = this.src;
+}
+
+function cerrarModal(){
+    modal.style.display = "none";
+}
+
+for(var i = 0; i < fotos.length; i++){
+    fotos[i].addEventListener("click",mostrarModal);
+}
+
+boton.addEventListener("click",cerrarModal);
